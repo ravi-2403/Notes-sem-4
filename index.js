b1207da3ec2b10fbf558725730dfb3256a98323d
Ravi Kumar <ravidtg3@gmail.com>
2021-06-26 08:18:57 +0530

    improved font style

---
+++ index.js
@@ -0,0 +1,7 @@
+const ele = document.getElementsByTagName('p')
+console.log(ele)
+
+ele[0].style.fontFamily="'Stylish', sans-serif"
+ele[1].style.fontFamily="'Stylish', sans-serif"
+ele[2].style.fontFamily="'Stylish', sans-serif"
+ele[3].style.fontFamily="'Stylish', sans-serif"
